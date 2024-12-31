497881f45c775014752309aa66d39e0319ed0650
NirZzZz <168575535+NirZzZz@users.noreply.github.com>
2024-12-31 16:20:59 +0200

    Added draw option and winning squares highliting

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, isWinningSquare }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button
+      className={`square ${isWinningSquare ? "winning-square" : ""}`}
+      onClick={onSquareClick}
+    >
       {value}
     </button>
   );
 }
 
 function Board({ xIsNext, squares, onPlay, move }) {
-  const winner = calculateWinner(squares);
+  const winnerInfo = calculateWinner(squares);
+  const winner = winnerInfo ? winnerInfo.winner : null;
+  const winningSquares = winnerInfo ? winnerInfo.line : [];
+
   let status;
   if (winner) {
     status = "Winner: " + winner;
+  } else if (squares.every(Boolean)) {
+    status = "It's a draw!";
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O") + ", You are at move #" + (move + 1);
   }
 
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (squares[i] || winner) {
       return;
     }
     const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
-    }
+    nextSquares[i] = xIsNext ? "X" : "O";
     onPlay(nextSquares);
   }
 
@@ -41,6 +45,7 @@ function Board({ xIsNext, squares, onPlay, move }) {
           key={index}
           value={squares[index]}
           onSquareClick={() => handleClick(index)}
+          isWinningSquare={winningSquares.includes(index)}
         />
       );
     }
@@ -128,7 +133,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], line: [a, b, c] };
     }
   }
   return null;
