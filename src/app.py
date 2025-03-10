07bdd4281dd3d62ad3eb0e52fe0616da0d2ed1d3
NirZzZz <168575535+NirZzZz@users.noreply.github.com>
2025-03-10 16:09:20 +0200

    commit

--- src/app.py
+++ src/app.py
@@ -47,32 +47,30 @@ def get_scoreboard():
     
     return jsonify(players)
 
-# Route to update the score (POST method)
 @app.route("/start_game", methods=["POST"])
 def start_game():
     data = request.get_json()
-    player_x = data.get('player_x')
-    player_o = data.get('player_o')
     winner = data.get('winner')
     loser = data.get('loser')
 
-    # Insert players if they do not exist, else update their scores
     conn = get_db_connection()
     cur = conn.cursor()
 
-    for player in [player_x, player_o]:
-        cur.execute("SELECT * FROM players WHERE player_name = %s", (player,))
-        player_data = cur.fetchone()
+    # Handle the winner
+    cur.execute("SELECT * FROM players WHERE player_name = %s", (winner,))
+    winner_data = cur.fetchone()
+    if winner_data:
+        cur.execute("UPDATE players SET score = score + 1 WHERE player_name = %s", (winner,))
+    else:
+        cur.execute("INSERT INTO players (player_name, score) VALUES (%s, %s)", (winner, 1))
 
-        if player_data:
-            # Update the score based on the winner and loser
-            if player == winner:
-                cur.execute("UPDATE players SET score = score + 1 WHERE player_name = %s", (player,))
-            elif player == loser:
-                cur.execute("UPDATE players SET score = score - 1 WHERE player_name = %s", (player,))
-        else:
-            # Insert new player with initial score 0
-            cur.execute("INSERT INTO players (player_name, score) VALUES (%s, %s)", (player, 0))
+    # Handle the loser
+    cur.execute("SELECT * FROM players WHERE player_name = %s", (loser,))
+    loser_data = cur.fetchone()
+    if loser_data:
+        cur.execute("UPDATE players SET score = score - 1 WHERE player_name = %s", (loser,))
+    else:
+        cur.execute("INSERT INTO players (player_name, score) VALUES (%s, %s)", (loser, -1))
 
     conn.commit()
     cur.close()
